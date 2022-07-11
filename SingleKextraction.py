a753548d684ccd67771ba3e7f38cc1293d4d2a84
GabrielLD <50905080+GabrielLD@users.noreply.github.com>
2022-07-11 15:00:18 +0200

    Add files via upload

--- SingleKextraction.py
+++ SingleKextraction.py
@@ -6,16 +6,23 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 from skimage import io
 from tools import cart2pol
-
-# fonctions
+from display import set_size, vcolors
+vcolor = vcolors(n=7)
+# fonctions fcd
 from fcd import *
-# from fcdgld import fcd_gld_hstar_series
-from demodulation import demodulation_gld_sp
+from demodulation import demodulation
 from save_deformation_field import save_deformation_field
 
+# kextraction
+from accum import accum
+from radialavg2 import radialavg2
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from kextraction import kextraction
+
 #%%
-date = '02052022'
-root = 'X:/Mina/Data/'
+date = '09062022'
+root = 'X:/GLD/Banquise/Data/'
 Directory = root+'d' + date
 print(Directory+'/d'+date+'*')
 folderlist = glob.glob(Directory+'/d'+date+'*')
@@ -26,10 +33,10 @@ exec(open(Directory + '/Parametres_jour.py').read())
 Nmax = 530
 hp = hpipx # distance pattern interface en pixels
 H = Hiopx  # ditance interface objectif en pixels
-facq = 51
-fpot = 20
+facq = 26.5
+fpot = 150
 #%%
-folder = folderlist[7]
+folder = folderlist[4]
 image_path = folder + '/image_sequence' # folder of the image sequence
 d = {} # initialisation of the dictionnary 
 
@@ -40,8 +47,6 @@ save_path = folder + '/resultats' # save folder
 if os.path.isdir(save_path) == False:
     os.mkdir(save_path)
 #%%
-
-
 
 # Reference image
 iref = io.imread_collection(os.path.join(image_path, "Image_ref*.tiff"),plugin = "tifffile", conserve_memory=True)[0]; # image de reference
@@ -61,12 +66,18 @@ Y = np.arange(0,ny)*fx
 T = np.arange(nt)/facq
 print('done')
 print('Computing demdulation of the field....')
-c, tf, etademod = demodulation_gld_sp(T,eta, fpot)
+c = demodulation(T,eta, fpot)
 print('Saving....')
 #save_deformation_field(c, save_path, filename = "\champ_deformation_detrend_demodulated_hxy_complexe_moyenne_sur_t")
 
 #%%
-fig, ax =plt.subplots(1,1)
+
+
+from scipy.io import loadmat
+c = loadmat(save_path + '/champ_demodule.mat')['output']
+
+#%%
+fig, ax =plt.subplots(1,1, figsize = set_size(width = 400, subplots = (1,1)))
 im = ax.pcolormesh(np.real(c))
 #plt.ticklabel_format(axis="x", style="sci", scilimits=(0,0))
 ax.set_xlabel(r'$x$ (cm)')
@@ -78,66 +89,18 @@ cbar.set_label(r'$\eta(x,y)$ (cm)')
 ax.set_aspect('equal', 'box')
 plt.tight_layout()
 #plt.savefig('./docs/source/images/Champ_deformation_n40.png', dpi = 300)
-# %%
-
-from accum import accum
-
-x0 = 200
-y0 = 300
-
-phase_locale = np.ones((2*fitlength,2*fitlength))*np.exp(1j*np.angle(c[x0,y0]))
-signal_local=np.zeros(phase_locale.shape)
-signal_local[:,:] = np.real(c[x0-fitlength:x0+fitlength, y0-fitlength:y0+fitlength]*phase_locale)
-
-def radialavg2(data, radial_step,x0,y0,m):
-    l = np.int(data.shape[0]/2)
-    x = np.arange(0,data.shape[1]) - data.shape[1]/2+(l-x0)
-    y = np.arange(0,data.shape[0]) - data.shape[0]/2+(l-y0)
-    [X,Y] = np.meshgrid(x,y)
-    [R, Theta] = cart2pol(X,Y)
-    Zinteger = np.zeros(R.shape)
-    Zinteger = R/radial_step
-    Zinteger = np.abs(X+1j*Y)/radial_step+1
-    Tics = accum(Zinteger.astype(int), np.abs(X+1j*Y), func = np.mean)
-    Average = accum(Zinteger.astype(int), data, func = np.mean)
-    return Tics, Average
-
-[r2,zr2] = radialavg2(signal_local, 1, fitlength+1, fitlength+1, m)
-plt.plot(r2,zr2)
 
 #%%
-[nx,ny] = c.shape
-cx = 0 
 pas = 1
-k2 = np.zeros((int((ny/fitlength)/step_ana), int((nx-fitlength)/step_ana)-1))
 step_ana = 1
-fitlength = 6
-m= 0
-phase_locale = np.ones((2*fitlength,2*fitlength))
-signal_local = np.zeros(phase_locale.shape)
-for x0 in range(fitlength, nx-fitlength+1, step_ana):
-    if np.mod(x0,60)==0:
-        print(str(np.round(x0*100/(nx-fitlength),0))+ ' % ')
-    cy = 0
-    for y0 in range(fitlength, ny-fitlength+1, step_ana):
-        phase_locale = np.ones((2*fitlength,2*fitlength))*np.exp(1j*np.angle(c[x0,y0]))
-        signal_local = np.real(c[x0-fitlength:x0+fitlength, y0-fitlength:y0+fitlength]*phase_locale)
-        [r2,zr2] = radialavg2(signal_local, 1, fitlength+1, fitlength+1, m)
-        xx = r2[0:fitlength]*pas
-        xx2 = np.concatenate((np.flipud(-xx),xx))
-        test = np.abs(zr2[0:fitlength])
-        test2 = np.concatenate((np.flipud(test),test))
-        pp = np.polyfit(xx2,test2,deg=2)
-        pp[0]=np.abs(pp[0])
-        pp[2]=np.abs(pp[2])
-        k2[cy,cx]=np.sqrt(4*pp[0]/pp[2])
-        cy=+1
-    cx =+ 1
+fitlength = 30
+kfield  = kextraction(c, fitlength, step_ana, step)
+
 print('done')
 
  #%%
 
-fig, ax = plt.subplots(1,1, figsize = (15,8))#set_size(width = 400, subplots = (1,1)))
+fig, ax = plt.subplots(1,1, figsize = set_size(width = 400, subplots = (1,1)))
 ax.pcolomesh(k2)
 
 
--- Turbotsice/display.py
+++ Turbotsice/display.py
@@ -34,12 +34,42 @@ def set_size(width, fraction=1, subplots=(1, 1)):
 
     # Golden ratio to set aesthetic figure height
     # https://disq.us/p/2940ij3
-    #golden_ratio = (5**.5 - 1) / 2
-    golden_ratio = .9
+    golden_ratio = (5**.5 - 1) / 2
+    #golden_ratio = .9
     # Figure width in inches
     fig_width_in = fig_width_pt * inches_per_pt
     # Figure height in inches
     fig_height_in = fig_width_in * golden_ratio * (subplots[0] / subplots[1])
     #fig_height_in = fig_width_in * .9 * (subplots[0] / subplots[1])
+    tex_fonts = {
+    # Use LaTeX to write all text
+    "text.usetex": True,
+    "font.family": "serif",
+    # Use 10pt font in plots, to match 10pt font in document
+    "axes.labelsize": 14,
+    "font.size": 14,
+    # Make the legend/label fonts a little smaller
+    "legend.fontsize":11,
+    "xtick.labelsize": 10,
+    "ytick.labelsize": 10
+    }
 
+    plt.rcParams.update(tex_fonts)
+    mpl.rc('text', usetex=True)
+    mpl.rcParams['text.latex.preamble'] = [
+    r'\usepackage{lmodern}', #lmodern: lateX font; tgheros: helvetica font
+    r'\usepackage{sansmath}', # math-font matching helvetica
+    r'\sansmath' # actually tell tex to use it!
+    r'\usepackage[scientific-notation=false]{siunitx}', # micro symbols
+    r'\sisetup{detect-all}', # force siunitx to use the fonts
+    ]
+    
     return (fig_width_in, fig_height_in)
+
+    ## FONTS
+
+
+## CHOIX DES COULEURS
+
+def vcolors(n=7):
+        return plt.cm.viridis(np.linspace(0,1,n))
